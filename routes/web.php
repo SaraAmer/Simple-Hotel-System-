8db7cb920083a11729a1217142b172e8b8cf59a1
Marwa Medhat <eng.marwamedhat2020@gmail.com>
2021-03-28 01:29:07 +0200

    receptionist-manage-approve-reserve-3

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,9 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use  App\Http\Controllers\ManageClientController;
 use  App\Http\Controllers\ClientReservationController;
 use App\Http\Controllers\ApprovedClientController;
-=======
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ManagersController;
-use App\Http\Controllers\ReceptionistsController;
-
->>>>>>> ddf8f369d3e1549937739acae341a6f97f14badd
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -56,42 +49,6 @@ Route::get('/receptionist/ClientReservation', [ManageClientController::class, 'i
 
 Route::get('/receptionist/ApprovedClient', [ApprovedClientController::class, 'index'])->name('Receptionist.ApprovedClient');
 
-
-
 Auth::routes();
-//Admin ONLY can....................................................
-Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::get('/managers', [ManagersController::class, 'index'])->name('managers.index');
-Route::post('/managers', [ManagersController::class, 'store'])->name('managers.store');
-
-Route::get('/managers/create', [ManagersController::class, 'create'])->name('managers.create');
-
-Route::get('/managers/{manager}/edit', [ManagersController::class, 'edit'])->name('managers.edit');
-
-Route::put('/managers/{manager}', [ManagersController::class, 'update'])->name('managers.update');
-
-Route::delete('/managers/{manager}', [ManagersController::class, 'destroy'])->name('managers.destroy');
-
-//Admin OR manager can........................
-
-Route::get('/receptionists', [ReceptionistsController::class, 'index'])->name('receptionists.index');
-Route::post('/receptionists', [ReceptionistsController::class, 'store'])->name('receptionists.store');
-
-Route::get('/receptionists/create', [ReceptionistsController::class, 'create'])->name('receptionists.create');
-
-Route::get('/receptionists/{receptionist}/edit', [ReceptionistsController::class, 'edit'])->name('receptionists.edit');
-
-Route::put('/receptionists/{receptionist}', [ReceptionistsController::class, 'update'])->name('receptionists.update');
-
-Route::delete('/receptionists/{receptionist}', [ReceptionistsController::class, 'destroy'])->name('receptionists.destroy');
-
-
-
-
-//Client
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/client', [App\Http\Controllers\ClientController::class, 'index'])->name('client');
-Route::get('/client/home', [App\Http\Controllers\ClientController::class, 'home'])->name('clientHome');
-Route::get('/client/reservation', [App\Http\Controllers\ClientController::class, 'reserve'])->name('clientReservation');
-Route::get('/client/invoice', [App\Http\Controllers\ClientController::class, 'viewInvoices'])->name('clientInvoice');
