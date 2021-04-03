6fdd1fd5f51ce7bc3b144fa0a67ece4298eb60fa
RaghdaMadiane <raghdamadiane@gmail.com>
2021-04-03 04:30:56 +0200

    eithar

---
+++ database/migrations/2021_04_03_015708_create_rooms_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->increments('room_number')->start_from(1000);
+            // $table->integer('floor_id');
+             $table->unsignedInteger('floor_id');///foriegn key to the floorstable
+
+             $table->foreign('floor_id')->nullable()
+
+             ->references('number')->on('floors')
+
+             ->onDelete('cascade');
+            //  $table->string('status');
+            $table->integer('capacity');
+            $table->float('price', 8, 3);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
--- resources/views/client/checkout.blade.php
+++ resources/views/client/checkout.blade.php
@@ -3,121 +3,121 @@
 @endsection
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                @if ($message = Session::get('success'))
-                <div class="custom-alerts alert alert-success fade in">
-                    <button type="button" class="close" data-dismiss="alert"></button>
-                    {{ $message }}
-                </div>
-                <?php Session::forget('success');?>
-                @endif
-                @if ($message = Session::get('error'))
-                <div class="custom-alerts alert alert-danger fade in">
-                    <button type="button" class="close" data-dismiss="alert"></button>
-                    {!! $message !!}
-                </div>
-                <?php Session::forget('error');?>
-                @endif
-                <div class="card mt-3 ml-5">
-                    <div class="card-header bg-light">
-                        <h3 class="card-title"><strong>Complete Payment</strong></h3>
+
+
+    @if ($message = Session::get('success'))
+    <div class="custom-alerts alert alert-success fade in">
+        <button type="button" class="close" data-dismiss="alert"></button>
+        {{ $message }}
+    </div>
+    <?php Session::forget('success');?>
+    @endif
+    @if ($message = Session::get('error'))
+    <div class="custom-alerts alert alert-danger fade in">
+        <button type="button" class="close" data-dismiss="alert"></button>
+        {!! $message !!}
+    </div>
+    <?php Session::forget('error');?>
+    @endif
+    <div class="container-fluid">
+        <div class="card mt-3 ml-5 mb-5 mr-5">
+            <div class="card-header bg-light">
+                <h3 class="card-title"><strong>Complete Payment</strong></h3>
+            </div>
+            <div class="card-body">
+                <form class="form-horizontal" method="POST" id="payment-form" role="form" action="{!! URL::route('paywithstripe', ['room' => $room , 'accompany_number' => $accompany_number]) !!}">
+                    {{ csrf_field() }}
+
+                    @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
                     </div>
-                <div class="card-body">
-                    <form class="form-horizontal" method="POST" id="payment-form" role="form" action="{!! URL::route('paywithstripe', ['room' => $room , 'accompany_number' => $accompany_number]) !!}">
-                        {{ csrf_field() }}
+                    @endif
+                    <div class="form-group{{ $errors->has('card_no') ? ' has-error' : '' }}">
+                        <label for="card_no" class="col-md-4 control-label">Card No</label>
+                        <div class="col-md-6">
+                            <input id="card_no" type="text" class="form-control" name="card_no" value="{{ old('card_no') }}" autofocus>
+                            @if ($errors->has('card_no'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('card_no') }}</strong>
+                            </span>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="form-group{{ $errors->has('ccExpiryMonth') ? ' has-error' : '' }}">
+                        <label for="ccExpiryMonth" class="col-md-4 control-label">Expiry Month</label>
+                        <div class="col-md-6">
+                            <input id="ccExpiryMonth" type="text" class="form-control" name="ccExpiryMonth" value="{{ old('ccExpiryMonth') }}" autofocus>
+                            @if ($errors->has('ccExpiryMonth'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('ccExpiryMonth') }}</strong>
+                            </span>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="form-group{{ $errors->has('ccExpiryYear') ? ' has-error' : '' }}">
+                        <label for="ccExpiryYear" class="col-md-4 control-label">Expiry Year</label>
+                        <div class="col-md-6">
+                            <input id="ccExpiryYear" type="text" class="form-control" name="ccExpiryYear" value="{{ old('ccExpiryYear') }}" autofocus>
+                            @if ($errors->has('ccExpiryYear'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('ccExpiryYear') }}</strong>
+                            </span>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="form-group{{ $errors->has('cvvNumber') ? ' has-error' : '' }}">
+                        <label for="cvvNumber" class="col-md-4 control-label">CVV No.</label>
+                        <div class="col-md-6">
+                            <input id="cvvNumber" type="text" class="form-control" name="cvvNumber" value="{{ old('cvvNumber') }}" autofocus>
+                            @if ($errors->has('cvvNumber'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('cvvNumber') }}</strong>
+                            </span>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="form-group{{ $errors->has('amount') ? ' has-error' : '' }}">
+                        <label for="amount" class="col-md-4 control-label">Price</label>
+                        <div class="col-md-6">
+                            <input id="amount" type="text" class="form-control" name="amount" value="{{ $reservedRoom['price'] }}$" autofocus readonly>
+                            @if ($errors->has('amount'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('amount') }}</strong>
+                            </span>
+                            @endif
+                        </div>
+                    </div>
 
-                        @if ($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
+                    <div class="form-group">
+                        <div class="col-md-6 col-md-offset-4">
+                            <button type="submit" class="btn btn-primary">
+                                Pay
+                            </button>
+                            <a href="{{ route('client.home') }}" class="btn btn-success">
+                                Return to home page
+                            </a>
                         </div>
-                        @endif
-                        <div class="form-group{{ $errors->has('card_no') ? ' has-error' : '' }}">
-                            <label for="card_no" class="col-md-4 control-label">Card No</label>
-                            <div class="col-md-6">
-                                <input id="card_no" type="text" class="form-control" name="card_no" value="{{ old('card_no') }}" autofocus>
-                                @if ($errors->has('card_no'))
-                                <span class="help-block">
-                                    <strong>{{ $errors->first('card_no') }}</strong>
-                                </span>
-                                @endif
-                            </div>
-                        </div>
-                        <div class="form-group{{ $errors->has('ccExpiryMonth') ? ' has-error' : '' }}">
-                            <label for="ccExpiryMonth" class="col-md-4 control-label">Expiry Month</label>
-                            <div class="col-md-6">
-                                <input id="ccExpiryMonth" type="text" class="form-control" name="ccExpiryMonth" value="{{ old('ccExpiryMonth') }}" autofocus>
-                                @if ($errors->has('ccExpiryMonth'))
-                                <span class="help-block">
-                                    <strong>{{ $errors->first('ccExpiryMonth') }}</strong>
-                                </span>
-                                @endif
-                            </div>
-                        </div>
-                        <div class="form-group{{ $errors->has('ccExpiryYear') ? ' has-error' : '' }}">
-                            <label for="ccExpiryYear" class="col-md-4 control-label">Expiry Year</label>
-                            <div class="col-md-6">
-                                <input id="ccExpiryYear" type="text" class="form-control" name="ccExpiryYear" value="{{ old('ccExpiryYear') }}" autofocus>
-                                @if ($errors->has('ccExpiryYear'))
-                                <span class="help-block">
-                                    <strong>{{ $errors->first('ccExpiryYear') }}</strong>
-                                </span>
-                                @endif
-                            </div>
-                        </div>
-                        <div class="form-group{{ $errors->has('cvvNumber') ? ' has-error' : '' }}">
-                            <label for="cvvNumber" class="col-md-4 control-label">CVV No.</label>
-                            <div class="col-md-6">
-                                <input id="cvvNumber" type="text" class="form-control" name="cvvNumber" value="{{ old('cvvNumber') }}" autofocus>
-                                @if ($errors->has('cvvNumber'))
-                                <span class="help-block">
-                                    <strong>{{ $errors->first('cvvNumber') }}</strong>
-                                </span>
-                                @endif
-                            </div>
-                        </div>
-                        <div class="form-group{{ $errors->has('amount') ? ' has-error' : '' }}">
-                            <label for="amount" class="col-md-4 control-label">Price</label>
-                            <div class="col-md-6">
-                                <input id="amount" type="text" class="form-control" name="amount" value="{{ $reservedRoom['price'] }}$" autofocus readonly>
-                                @if ($errors->has('amount'))
-                                <span class="help-block">
-                                    <strong>{{ $errors->first('amount') }}</strong>
-                                </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Pay
-                                </button>
-                                <a href="{{ route('client.home') }}" class="btn btn-success">
-                                    Return to home page
-                                </a>
-                            </div>
-                        </div>
+                    </div>
 
 
 
-                    </form>
+                </form>
 
-                </div>
             </div>
         </div>
 
+
+
+
+
     </div>
+</div>
 
 
-
-
-
-
-    @endsection
+@endsection
 
